acd2edf3c5130af555beb65f9c7ff62ec7b47a00
Ross Messer <rmesser@perennial.local>
2018-05-02 14:34:01 -0500

    additional changes

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var keys = require("./keys.js");
+// var keys = require("./keys.js");
 
 var connection = mysql.createConnection({
   port: 3306,
